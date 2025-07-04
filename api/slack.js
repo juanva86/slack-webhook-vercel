f96bb635d90f43b4ac3e9d51152db2888f7ec83a
juanva86 <juan.cortes.fabregas@gmail.com>
2025-07-04 16:57:14 +0200

    slack.js 1.0.1

--- api/slack.js
+++ api/slack.js
@@ -2,6 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
   console.log("✅ Endpoint recibido correctamente");
+  //tweak for redeploy 1.0.1
 
   const { nombre, email, canal, resumen } = req.body;
 
